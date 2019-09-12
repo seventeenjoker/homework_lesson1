fc29f2b2fe805ed36529322bf45fa02ed1de3ec1
Victoria Kiseleva <seventeen.joker@gmail.com>
2019-09-12 18:26:57 +0200

    fixed homework2

--- stars_bot/bot.py
+++ stars_bot/bot.py
@@ -12,7 +12,8 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 def greet_user(bot, update):
     text = 'Вызван /start'
     logging.info(text)
-    user_text = 'Hello stranger! Here you can get information about Planet in constellation on today. Tell me now which planet you wanna know about? Use /planet planet_name ;)'
+    user_text = 'Hello stranger! Here you can get information about Planet in constellation on today. '
+                'Tell me now which planet you wanna know about? Use /planet planet_name ;)'
     update.message.reply_text(user_text)
 
 def check_the_sign(bot, update):
@@ -31,42 +32,12 @@ def check_the_sign(bot, update):
         'Venus': ephem.Venus
     }
     message = update.message.text
-    planet = message.split(' ')
-    planet_new = planet[1].capitalize()
-    print(planet_new)
+    command, planet = message.split()
+    planet_new = planet.capitalize()
     misunderstanding = "I didn't get you, sorry. Try '/planet sun'"
     error_message = 'Ooops...something went wrong...'
-    if planet_new in planets_dic:
+    if planets_dic.get(planet_new, 0) in planets_dic:
         update.message.reply_text(ephem.constellation(planets_dic[planet_new](today)))
-    # try:
-    #     if planet[1] == 'Mars':
-    #         planet = ephem.Mars(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Moon':
-    #         planet = ephem.Moon(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Saturn':
-    #         planet = ephem.Moon(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Jupiter':
-    #         planet = ephem.Jupiter(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Uranus':
-    #         planet = ephem.Uranus(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Neptune':
-    #         planet = ephem.Neptune(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Pluto':
-    #         planet = ephem.Pluto(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     elif planet[1] == 'Sun':
-    #         planet = ephem.Sun(today)
-    #         update.message.reply_text(ephem.constellation(planet))
-    #     else:
-    #         update.message.reply_text(misunderstanding)
-    # except:
-    #     logging.info(error_message)
 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
