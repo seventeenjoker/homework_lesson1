b85dc0b7c4946a90d60e1057804d682cbb08e93c
Victoria Kiseleva <kiseleva.victoria.u@gmail.com>
2019-09-16 12:02:04 +0200

    add homework with csv

---
+++ lesson2_csv.py
@@ -0,0 +1,25 @@
+import csv
+
+'''
+Задание
+Создайте список словарей:
+        [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+Запишите содержимое списка словарей в файл в формате csv
+'''
+
+persons_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+
+with open('new_csv_file.csv', 'w', encoding = 'utf-8') as f:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(f, fields, delimiter = ';')
+    writer.writeheader()
+    for person in persons_list:
+        writer.writerow(person)
